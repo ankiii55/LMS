e175fda595e959e2b4fbf947749fb534320528e1
Ankit Choudhary <113855736+ankiii55@users.noreply.github.com>
2025-04-02 13:51:18 +0530

    Add files via upload

---
+++ library_management_system/library_management_system/src/main/java/com/ankit/library_management_system/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.ankit.library_management_system.controller;
+
+import com.ankit.library_management_system.entity.User;
+import com.ankit.library_management_system.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.findAll();
+    }
+
+    @PostMapping
+    public User addUser(@RequestBody User user) {
+        return userService.save(user);
+    }
+}
